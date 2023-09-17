e6fb4084420c67bdb70859f315b48c1b237411af
Dave Gymer <github@davegymer.org>
2023-09-17 17:35:18 +0100

    Create ajbell-prices.user.js

---
+++ ajbell-prices.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        ajbell-prices
+// @namespace   org.davegymer
+// @description Add copy function to YouInvest prices
+// @match       https://www.ajbell.co.uk/platform/*
+// @version     20230917.00
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require     https://raw.githubusercontent.com/MikeMcl/decimal.js/master/decimal.js
+// @grant       GM_addStyle
+// @updateURL    https://github.com/gizmo71/CurrencyCorrectingUserScripts/raw/main/ajbell-prices.user.js
+// @downloadURL  https://github.com/gizmo71/CurrencyCorrectingUserScripts/raw/main/ajbell-prices.user.js
+// ==/UserScript==
+
+function convert(jNode) {
+    var price = jNode.text().match(/^(\* |)((?:\d+,)*\d+(?:\.\d+)?)\s+GBP\s*$/);
+//console.log("is " + jNode.text() + " now " + price);
+    if (price) {
+        jNode.css("background", "lightgoldenrodyellow");
+        var poundsPence = new Decimal(price[2]);
+        jNode.html(price[1] + "£" + poundsPence + "<a href='javascript:navigator.clipboard.writeText(&quot;" + poundsPence + "&quot;);'>📋</a>");
+    }
+}
+//console.log("gonna wait");
+waitForKeyElements('div[class^="price-"] > div', convert); // [class="no-wrap"]
